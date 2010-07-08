d5b58daa09ab548fb16cc7ee72316b68e1ef6c14
brian.dilley <brian.dilley@16309028-0b48-8e1c-506f-6718288bf2b3>
2010-07-08 23:43:02 +0000

    added some dao stuff added some spring support for stuff stuff and stuff stuff... and stuff.

---
+++ src/main/java/com/googlecode/mjorm/spring/XmlDescriptorObjectMapperFactoryBean.java
@@ -0,0 +1,59 @@
+package com.googlecode.mjorm.spring;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.core.io.Resource;
+
+import com.googlecode.mjorm.XmlDescriptorObjectMapper;
+
+public class XmlDescriptorObjectMapperFactoryBean
+	extends AbstractFactoryBean<XmlDescriptorObjectMapper> {
+
+	private List<Resource> xmlResources = new ArrayList<Resource>();
+	private List<File> xmlFiles = new ArrayList<File>();
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected XmlDescriptorObjectMapper createInstance()
+		throws Exception {
+		XmlDescriptorObjectMapper mapper = new XmlDescriptorObjectMapper();
+		for (Resource resource : xmlResources) {
+			InputStream ips = resource.getInputStream();
+			mapper.addXmlObjectDescriptor(ips);
+			ips.close();
+		}
+		for (File file : xmlFiles) {
+			mapper.addXmlObjectDescriptor(file);
+		}
+		return mapper;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<?> getObjectType() {
+		return XmlDescriptorObjectMapper.class;
+	}
+
+	/**
+	 * @param xmlResources the xmlResources to set
+	 */
+	public void setXmlResources(List<Resource> xmlResources) {
+		this.xmlResources = xmlResources;
+	}
+
+	/**
+	 * @param xmlFiles the xmlFiles to set
+	 */
+	public void setXmlFiles(List<File> xmlFiles) {
+		this.xmlFiles = xmlFiles;
+	}
+
+}
