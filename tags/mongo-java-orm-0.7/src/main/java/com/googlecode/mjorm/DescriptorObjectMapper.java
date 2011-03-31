268b8ee75f750727989a07a232c8507bd823a468
brian.dilley <brian.dilley@16309028-0b48-8e1c-506f-6718288bf2b3>
2011-03-31 00:24:05 +0000

    [maven-release-plugin]  copy for tag mongo-java-orm-0.7

---
+++ tags/mongo-java-orm-0.7/src/main/java/com/googlecode/mjorm/DescriptorObjectMapper.java
@@ -0,0 +1,21 @@
+package com.googlecode.mjorm;
+
+import com.googlecode.mjorm.jot.DescriptorTranslator;
+
+/**
+ * Abstract class that uses {@link ObjectDescriptor}s and
+ * {@link PropertyDescriptor}s to map objects to and from
+ * mongo's {@link DBObject}s.
+ */
+public class DescriptorObjectMapper
+	extends AbstractObjectMapper {
+
+	/**
+	 * Registers a new {@link ObjectDescriptor}.
+	 * @param descriptor the {@link ObjectDescriptor]
+	 */
+	protected void registerObjectDescriptor(ObjectDescriptor descriptor) {
+		super.registerConverter(new DescriptorTranslator(descriptor));
+	}
+
+}
