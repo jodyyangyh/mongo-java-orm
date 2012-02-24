8cbd5c1446d245d259ba01428293ea362eae6a0c
brian.dilley <brian.dilley@16309028-0b48-8e1c-506f-6718288bf2b3>
2012-02-24 21:40:14 +0000

    [maven-release-plugin]  copy for tag mongo-java-orm-0.12

---
+++ tags/mongo-java-orm-0.12/src/main/java/com/googlecode/mjorm/ObjectMapper.java
@@ -0,0 +1,31 @@
+package com.googlecode.mjorm;
+
+import com.mongodb.DBObject;
+
+/**
+ * The {@code ObjectMapper} is responsible for translating
+ * objects to and from mongo's {@link DBObject}s.
+ */
+public interface ObjectMapper {
+
+	/**
+	 * Translates the given {@link DBObject} into a java object.
+	 * @param <T> the type
+	 * @param dbObject the {@link DBObject}
+	 * @param objectClass the {@link Class} of the object to translate to
+	 * @throws MappingException on error
+	 * @return the java object
+	 */
+	<T> T mapFromDBObject(DBObject dbObject, Class<T> objectClass)
+		throws MappingException;
+
+	/**
+	 * Translates the given java object into a {@link DBObject}.
+	 * @param <T> the type
+	 * @param object the java object
+	 * @throws MappingException on error
+	 * @return the {@link DBObject}
+	 */
+	<T> DBObject mapToDBObject(T object)
+		throws MappingException;
+}
