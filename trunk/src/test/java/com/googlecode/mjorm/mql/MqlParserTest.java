e68df5d792b2d088bd6f4047c1b783ed68ee0194
brian.dilley@gmail.com <brian.dilley@gmail.com@16309028-0b48-8e1c-506f-6718288bf2b3>
2012-05-02 22:27:38 +0000

    work on MQL grammar

---
+++ trunk/src/test/java/com/googlecode/mjorm/mql/MqlParserTest.java
@@ -0,0 +1,41 @@
+package com.googlecode.mjorm.mql;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+
+public class MqlParserTest {
+
+
+	public MqlParser createParser(String resource)
+		throws IOException {
+		return createParser(getClass().getResourceAsStream(resource));
+	}
+
+
+	public MqlParser createParser(InputStream ips)
+		throws IOException {
+		MqlLexer lexer 				= new MqlLexer(new ANTLRInputStream(ips));
+		CommonTokenStream tokens 	= new CommonTokenStream(lexer);
+		MqlParser parser 			= new MqlParser(tokens);
+		return parser;
+	}
+
+	@Test
+	public void test()
+		throws Exception {
+		MqlParser parser = createParser("/com/googlecode/mjorm/mql/test.mql");
+		try {
+			parser.something();
+		} catch (RecognitionException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
