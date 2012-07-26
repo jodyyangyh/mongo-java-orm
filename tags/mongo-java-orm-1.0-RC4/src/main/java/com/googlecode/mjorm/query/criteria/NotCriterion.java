7ae1a5d78a8bc7940b6827bef4ffc78b9a35ef72
brian.dilley@gmail.com <brian.dilley@gmail.com@16309028-0b48-8e1c-506f-6718288bf2b3>
2012-07-26 22:13:14 +0000

    [maven-release-plugin]  copy for tag mongo-java-orm-1.0-RC4

---
+++ tags/mongo-java-orm-1.0-RC4/src/main/java/com/googlecode/mjorm/query/criteria/NotCriterion.java
@@ -0,0 +1,26 @@
+package com.googlecode.mjorm.query.criteria;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+public class NotCriterion
+	extends FieldCriterion {
+
+	public NotCriterion(String fieldName, Criterion criterion) {
+		super(fieldName, criterion);
+	}
+
+	public NotCriterion(FieldCriterion criterion) {
+		this(criterion.getFieldName(), criterion.getCriterion());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public DBObject toQueryObject() {
+		return new BasicDBObject(getFieldName(),
+				new BasicDBObject("$not", getCriterion().toQueryObject()));
+	}
+
+}
