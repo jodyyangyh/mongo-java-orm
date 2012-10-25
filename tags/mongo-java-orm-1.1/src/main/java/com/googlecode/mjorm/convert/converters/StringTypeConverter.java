1b1a90de9080b63ad4d3346373587b380d3dd2f3
brian.dilley@gmail.com <brian.dilley@gmail.com@16309028-0b48-8e1c-506f-6718288bf2b3>
2012-10-25 05:22:30 +0000

    [maven-release-plugin]  copy for tag mongo-java-orm-1.1

---
+++ tags/mongo-java-orm-1.1/src/main/java/com/googlecode/mjorm/convert/converters/StringTypeConverter.java
@@ -0,0 +1,20 @@
+package com.googlecode.mjorm.convert.converters;
+
+import com.googlecode.mjorm.convert.ConversionContext;
+import com.googlecode.mjorm.convert.ConversionException;
+import com.googlecode.mjorm.convert.JavaType;
+import com.googlecode.mjorm.convert.TypeConverter;
+
+public class StringTypeConverter
+	implements TypeConverter<Object, String> {
+
+	public boolean canConvert(Class<?> sourceClass, Class<?> targetClass) {
+		return String.class.isAssignableFrom(targetClass);
+	}
+
+	public String convert(Object source, JavaType targetType, ConversionContext context)
+		throws ConversionException {
+		return source.toString();
+	}
+
+}
